7696666afae97da9eefc7b7a10b0369b4dda7542
Sunil D S <sunil3590@gmail.com>
2018-01-29 18:58:11 +0530

    more clean up

--- assets/js/map-script.js
+++ assets/js/map-script.js
@@ -9,7 +9,7 @@
                     zoom: 11,
 
                     // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(12.9438075, 77.501278), // New York
+                    center: new google.maps.LatLng(12.9438075, 77.501278), // Bangalore
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
@@ -25,7 +25,7 @@
 
                 // Let's also add a marker while we're at it
                 var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(40.6700, -73.9400),
+                    position: new google.maps.LatLng(12.9438075, 77.501278),
                     map: map,
                     title: 'Snazzy!'
                 });
